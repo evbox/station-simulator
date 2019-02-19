003a16b37f847fd8544e29797fde21e87c214cec
Aleksandr Nesterov <aleksandr.nesterov@ev-box.com>
2019-02-19 09:38:06 +0100

    deleted unused classes

--- simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/SubscriptionContext.java
+++
@@ -1,9 +0,0 @@
-package com.evbox.everon.ocpp.simulator.station;
-
-import lombok.Value;
-
-@Value
-public class SubscriptionContext<REQ, RES> {
-    Subscriber<REQ, RES> subscriber;
-    REQ request;
-}
