13b80006cb9017cb6fd826ed1e3d795fdee14ca9
Aleksandr Nesterov <aleksandr.nesterov@ev-box.com>
2019-03-13 14:08:10 +0100

    removed duplicated exceptions

--- simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/exception/ParseException.java
+++
@@ -1,7 +0,0 @@
-package com.evbox.everon.ocpp.simulator.station.exception;
-
-public class ParseException extends RuntimeException {
-    public ParseException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-}
