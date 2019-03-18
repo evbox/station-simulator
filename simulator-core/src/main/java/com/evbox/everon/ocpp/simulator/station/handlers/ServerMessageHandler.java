8062f1aaee5473a6f2db17601f49a46f0960f478
Dragan Markovic <dragan.markovic@ev-box.com>
2019-03-18 17:07:45 +0100

    Add basic base report handler response

--- simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/handlers/ServerMessageHandler.java
+++ simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/handlers/ServerMessageHandler.java
@@ -51,6 +51,7 @@ public class ServerMessageHandler implements MessageHandler<String> {
                 .put(SetVariablesRequest.class, new SetVariablesRequestHandler(stationComponentsHolder, stationMessageSender))
                 .put(ResetRequest.class, new ResetRequestHandler(stationState, stationMessageSender))
                 .put(ChangeAvailabilityRequest.class, new ChangeAvailabilityRequestHandler(new AvailabilityManager(stationState, stationMessageSender)))
+                .put(GetBaseReportHandler.class, new GetBaseReportHandler(stationComponentsHolder, stationMessageSender))
                 .build();
     }
 
