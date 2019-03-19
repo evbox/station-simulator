9894253bbcc355dcbce41787fef5e7bdbf0f594a
Dragan Markovic <dragan.markovic@ev-box.com>
2019-03-19 13:08:44 +0100

    Rename GetBaseReportRequestHandler

---
+++ simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/handlers/ocpp/GetBaseReportRequestHandler.java
@@ -0,0 +1,38 @@
+package com.evbox.everon.ocpp.simulator.station.handlers.ocpp;
+
+import com.evbox.everon.ocpp.simulator.station.StationMessageSender;
+import com.evbox.everon.ocpp.simulator.station.component.StationComponentsHolder;
+import com.evbox.everon.ocpp.v20.message.station.GetBaseReportRequest;
+import com.evbox.everon.ocpp.v20.message.station.GetBaseReportResponse;
+import com.google.common.collect.ImmutableList;
+
+import static com.evbox.everon.ocpp.v20.message.station.GetBaseReportResponse.Status.ACCEPTED;
+import static com.evbox.everon.ocpp.v20.message.station.GetBaseReportResponse.Status.NOT_SUPPORTED;
+import static com.google.common.collect.ImmutableList.of;
+
+public class GetBaseReportRequestHandler implements OcppRequestHandler<GetBaseReportRequest> {
+
+    private static final ImmutableList<String> SUPPORTED_VARIABLES = of("HeartbeatInterval");
+    private static final Integer UNKONWN_ID = Integer.valueOf(0);
+
+    private final StationMessageSender stationMessageSender;
+    private final StationComponentsHolder stationComponentsHolder;
+
+
+    public GetBaseReportRequestHandler(StationComponentsHolder stationComponentsHolder, StationMessageSender stationMessageSender) {
+        this.stationMessageSender = stationMessageSender;
+        this.stationComponentsHolder = stationComponentsHolder;
+    }
+
+    @Override
+    public void handle(String callId, GetBaseReportRequest request) {
+        switch (request.getReportBase()) {
+            case FULL_INVENTORY:
+                stationMessageSender.sendCallResult(callId, new GetBaseReportResponse().withStatus(ACCEPTED));
+            case CONFIGURATION_INVENTORY:
+                stationMessageSender.sendCallResult(callId, new GetBaseReportResponse().withStatus(ACCEPTED));
+            default: // SUMMARY_INVENTORY
+                stationMessageSender.sendCallResult(callId, new GetBaseReportResponse().withStatus(NOT_SUPPORTED));
+        }
+    }
+}
