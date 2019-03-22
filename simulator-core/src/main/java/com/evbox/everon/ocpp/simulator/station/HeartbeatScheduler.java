7e160b2a7b780c6db82325c051b28a8062aa8744
romanvlasenko <roman.vlasenko@ev-box.com>
2019-03-22 08:53:31 +0100

    SPE-657 Heartbeat gives nullptr exception on startup, and also doesn't respect configuration - Moved task scheduling to constructor to be executed only once

--- simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/HeartbeatScheduler.java
+++ simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/HeartbeatScheduler.java
@@ -4,7 +4,6 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -14,16 +13,15 @@ public class HeartbeatScheduler {
     private static final int TASK_DELAY_IN_SECONDS = 1;
 
     private HeartbeatSenderTask heartbeatTask;
-    private ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
     public HeartbeatScheduler(StationState stationState, StationMessageSender stationMessageSender) {
         this.heartbeatTask = new HeartbeatSenderTask(stationState, stationMessageSender);
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
+                heartbeatTask, TASK_DELAY_IN_SECONDS, TASK_DELAY_IN_SECONDS, TimeUnit.SECONDS);
     }
 
     public void updateHeartbeat(int heartbeatInterval) {
         log.debug("Updating heartbeat to {} sec.", heartbeatInterval);
         heartbeatTask.updateHeartBeatInterval(heartbeatInterval);
-        scheduledExecutorService.scheduleAtFixedRate(
-                heartbeatTask, TASK_DELAY_IN_SECONDS, TASK_DELAY_IN_SECONDS, TimeUnit.SECONDS);
     }
 }
