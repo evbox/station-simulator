78b0b39fd35df0eb757517ff85531b7573ffd541
Mounir Karoud <mounir.karoud@ev-box.com>
2019-08-12 14:36:49 +0200

    EVS-10: Improved stat command output readability

--- simulator-core/src/test/java/com/evbox/everon/ocpp/simulator/station/StationTest.java
+++ simulator-core/src/test/java/com/evbox/everon/ocpp/simulator/station/StationTest.java
@@ -1,7 +1,10 @@
 package com.evbox.everon.ocpp.simulator.station;
 
 import com.evbox.everon.ocpp.simulator.configuration.SimulatorConfiguration;
+import com.evbox.everon.ocpp.simulator.station.actions.Unplug;
 import com.evbox.everon.ocpp.simulator.websocket.WebSocketClient;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +12,12 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.util.*;
+
 import static com.evbox.everon.ocpp.mock.ReflectionUtils.injectMock;
 import static com.evbox.everon.ocpp.mock.constants.StationConstants.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 
@@ -35,6 +42,24 @@ class StationTest {
         station = new Station(stationConfiguration);
 
         injectMock(station, "webSocketClient", webSocketClientMock);
+    }
+
+    @Test
+    void verifyStatOutputFields() throws IOException {
+        station.refreshStateView();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String statOutput = station.getStateView().toString();
+        Set<String> expectedFields = new HashSet<>(Arrays.asList("heartbeatInterval", "evses", "currentTime"));
+
+        JsonNode node = objectMapper.readTree(statOutput);
+        Iterator<String> fieldsIterator = node.fieldNames();
+        int fieldsCount = 0;
+        while (fieldsIterator.hasNext()) {
+            assertThat(expectedFields.contains(fieldsIterator.next())).isTrue();
+            fieldsCount++;
+        }
+        assertThat(fieldsCount).isEqualTo(expectedFields.size());
     }
 
     @Test
