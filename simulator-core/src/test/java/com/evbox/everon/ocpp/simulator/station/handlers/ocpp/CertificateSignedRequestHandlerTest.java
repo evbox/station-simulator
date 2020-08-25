1b9b630418391486b44aa8bc70bf756e3aaf38ab
thomas.fitzpatrick <thomas.fitzpatrick@ev-box.com>
2020-08-25 20:28:30 +0200

    EVS-1283,EVS-1388: Migration to OCPP 2.0.1: CertificateSignedRequestHandlerTest

--- simulator-core/src/test/java/com/evbox/everon/ocpp/simulator/station/handlers/ocpp/CertificateSignedRequestHandlerTest.java
+++ simulator-core/src/test/java/com/evbox/everon/ocpp/simulator/station/handlers/ocpp/CertificateSignedRequestHandlerTest.java
@@ -3,8 +3,10 @@ package com.evbox.everon.ocpp.simulator.station.handlers.ocpp;
 import com.evbox.everon.ocpp.common.CiString;
 import com.evbox.everon.ocpp.simulator.station.StationMessageSender;
 import com.evbox.everon.ocpp.simulator.station.StationStore;
-import com.evbox.everon.ocpp.v20.message.station.CertificateSignedRequest;
-import com.evbox.everon.ocpp.v20.message.station.CertificateSignedResponse;
+import com.evbox.everon.ocpp.v201.message.station.CertificateSignedRequest;
+import com.evbox.everon.ocpp.v201.message.station.CertificateSignedResponse;
+import com.evbox.everon.ocpp.v201.message.station.CertificateSignedStatus;
+import com.evbox.everon.ocpp.v201.message.station.CertificateSigningUse;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import org.junit.jupiter.api.BeforeAll;
@@ -30,7 +32,8 @@ import java.util.concurrent.TimeUnit;
 
 import static com.evbox.everon.ocpp.mock.constants.StationConstants.DEFAULT_MESSAGE_ID;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -61,50 +64,50 @@ public class CertificateSignedRequestHandlerTest {
     @Test
     @DisplayName("Expired certificate should be rejected")
     void verifyExpiredCertificateIsRejected() {
-        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCert(stringToCiStringsList(expiredCertificate)));
+        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCertificateChain(new CiString.CiString10000(expiredCertificate)));//TODO check that certificate is actually a "chain"
 
         verify(stationMessageSenderMock).sendCallResult(any(), messageCaptor.capture());
-        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedResponse.Status.REJECTED.value());
+        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedStatus.REJECTED.value());
         assertThat(Optional.ofNullable(requestHandler.getScheduledFuture())).isEmpty();
     }
 
     @Test
     @DisplayName("Empty certificate should be rejected")
     void verifyEmptyCertificateIsRejected() {
-        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCert(stringToCiStringsList("NotACert")));
+        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCertificateChain(new CiString.CiString10000("NotACert")));//TODO check that certificate is actually a "chain"
 
         verify(stationMessageSenderMock).sendCallResult(any(), messageCaptor.capture());
-        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedResponse.Status.REJECTED.value());
+        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedStatus.REJECTED.value());
         assertThat(Optional.ofNullable(requestHandler.getScheduledFuture())).isEmpty();
     }
 
     @Test
     @DisplayName("Certificate with wrong serial number should be rejected")
     void verifyCertificateWithInvalidSerialIsRejected() {
-        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCert(stringToCiStringsList(wrongSerialCertificate)));
+        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCertificateChain(new CiString.CiString10000(wrongSerialCertificate)));//TODO check that certificate is actually a "chain"
 
         verify(stationMessageSenderMock).sendCallResult(any(), messageCaptor.capture());
-        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedResponse.Status.REJECTED.value());
+        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedStatus.REJECTED.value());
         assertThat(Optional.ofNullable(requestHandler.getScheduledFuture())).isEmpty();
     }
 
     @Test
     @DisplayName("V2G certificate type should be rejected")
     void verifyV2GCertificateTypeIsRejected() {
-        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withTypeOfCertificate(CertificateSignedRequest.TypeOfCertificate.V_2_G_CERTIFICATE).withCert(stringToCiStringsList(wrongSerialCertificate)));
+        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCertificateType(CertificateSigningUse.V_2_G_CERTIFICATE).withCertificateChain(new CiString.CiString10000(wrongSerialCertificate)));//TODO check that certificate is actually a "chain"
 
         verify(stationMessageSenderMock).sendCallResult(any(), messageCaptor.capture());
-        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedResponse.Status.REJECTED.value());
+        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedStatus.REJECTED.value());
         assertThat(Optional.ofNullable(requestHandler.getScheduledFuture())).isEmpty();
     }
 
     @Test
     @DisplayName("Correct certificate type should be accepted")
     void verifyValidCertificateTypeIsCorrectlySet() {
-        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCert(stringToCiStringsList(validCertificate)));
+        requestHandler.handle(DEFAULT_MESSAGE_ID, new CertificateSignedRequest().withCertificateChain(new CiString.CiString10000(validCertificate)));//TODO check that certificate is actually a "chain"
 
         verify(stationMessageSenderMock).sendCallResult(any(), messageCaptor.capture());
-        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedResponse.Status.ACCEPTED.value());
+        assertThat(messageCaptor.getValue().getStatus().value()).isEqualTo(CertificateSignedStatus.ACCEPTED.value());
         verify(stationStoreMock).setStationCertificate(any());
         verify(stationStoreMock).setStationCertificateChain(argThat(chain -> chain.size() == 1));
 
@@ -118,6 +121,8 @@ public class CertificateSignedRequestHandlerTest {
         assertThat(triggerDate.getYear()).isEqualTo(2029);
     }
 
+    //TODO consider removing given new certificate chain parameter OCPP 2.0 to OCPP 2.0.1
+    @SuppressWarnings("unused")
     private List<CiString.CiString5500> stringToCiStringsList(String certificate) {
         List<CiString.CiString5500> result = new ArrayList<>();
         Arrays.stream(certificate.split("\n")).forEach(c -> result.add(new CiString.CiString5500(c)));
