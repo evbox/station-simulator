176229d42847a5c0f0919f5e696f564889782555
thomas.fitzpatrick <thomas.fitzpatrick@ev-box.com>
2020-08-25 19:53:19 +0200

    EVS-1283,EVS-1388: Migration to OCPP 2.0.1: imports

--- simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/handlers/ocpp/support/AvailabilityStateMapper.java
+++ simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/handlers/ocpp/support/AvailabilityStateMapper.java
@@ -1,15 +1,15 @@
 package com.evbox.everon.ocpp.simulator.station.handlers.ocpp.support;
 
 import com.evbox.everon.ocpp.simulator.station.evse.EvseStatus;
-import com.evbox.everon.ocpp.v20.message.station.ChangeAvailabilityRequest.OperationalStatus;
+import com.evbox.everon.ocpp.v201.message.station.OperationalStatus;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.Map;
 
 import static com.evbox.everon.ocpp.simulator.station.evse.EvseStatus.AVAILABLE;
 import static com.evbox.everon.ocpp.simulator.station.evse.EvseStatus.UNAVAILABLE;
-import static com.evbox.everon.ocpp.v20.message.station.ChangeAvailabilityRequest.OperationalStatus.INOPERATIVE;
-import static com.evbox.everon.ocpp.v20.message.station.ChangeAvailabilityRequest.OperationalStatus.OPERATIVE;
+import static com.evbox.everon.ocpp.v201.message.station.OperationalStatus.INOPERATIVE;
+import static com.evbox.everon.ocpp.v201.message.station.OperationalStatus.OPERATIVE;
 import static java.util.Objects.nonNull;
 
 /**
