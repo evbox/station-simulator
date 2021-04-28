02c33cbbc101f14eecfc76bd62f942dd828d2f5c
Abith Ganadahole <abith.ganadahole@ev-box.com>
2021-04-28 13:45:01 +0200

    Add 201 mandatory attribute for NotifyEventRequest

--- simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/schedulers/AlertEventSenderTask.java
+++ simulator-core/src/main/java/com/evbox/everon/ocpp/simulator/station/schedulers/AlertEventSenderTask.java
@@ -2,10 +2,7 @@ package com.evbox.everon.ocpp.simulator.station.schedulers;
 
 import com.evbox.everon.ocpp.common.CiString;
 import com.evbox.everon.ocpp.simulator.station.StationMessageSender;
-import com.evbox.everon.ocpp.v201.message.station.Component;
-import com.evbox.everon.ocpp.v201.message.station.EventData;
-import com.evbox.everon.ocpp.v201.message.station.EventTrigger;
-import com.evbox.everon.ocpp.v201.message.station.Variable;
+import com.evbox.everon.ocpp.v201.message.station.*;
 import lombok.AllArgsConstructor;
 
 import java.time.ZoneOffset;
@@ -31,6 +28,7 @@ public class AlertEventSenderTask implements Runnable {
                 .withActualValue(new CiString.CiString2500("999"))
                 .withCleared(true)
                 .withComponent(new Component().withName(new CiString.CiString50("component")))
+                .withEventNotificationType(EventNotification.CUSTOM_MONITOR)
                 .withVariable(new Variable().withName(new CiString.CiString50("variable")));
     }
 }
